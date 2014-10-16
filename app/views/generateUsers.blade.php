cb51fe3a76eca4abe9d78767b7100fa492aafc87
tom <tbeikman@fas.harvard.edu>
2014-10-15 20:49:47 -0400

    Add a form to generate users

--- app/views/generateUsers.blade.php
+++ app/views/generateUsers.blade.php
@@ -6,6 +6,29 @@
 
 @section('bodyContent_1')
      This is the Random Users Generator 
+
+     <!-- 1. Text box for a number 
+             2. Two checkboxes
+             3. Button to make it happen
+                 4 ui controls total
+     -->
+    <form action="{{ url('generateUsers') }}" method="POST">
+        <div>
+            <label for='numberOfUsers'>Please enter the number for how many users to generate.  Maximum value is allowed is 99.</label><br>
+            <input type="text" maxlength=2 size=1 name='numberOfUsers' id='numberOfUsers'>
+            <br>
+            <br>
+            <label for='appendANumber'>Include a Birthdate?</label><br>
+            <input type='checkbox' name='appendANumber' id='appendANumber'>
+            <br>
+            <br>            
+            <label for='appendACharacter'>Include a Profile?</label><br>
+            <input type='checkbox' name='appendACharacter' id='appendACharacter'>
+            <br>
+            <br>
+            <input class="button" type='submit' value="Let's generate some People!">
+        </div>
+     </form>
 @stop
 
 
