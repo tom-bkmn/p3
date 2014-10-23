d262972648735fa8a86c16aaa801cd453ef7cffe
tom <tbeikman@fas.harvard.edu>
2014-10-22 20:21:55 -0400

    HTML doctype update for w3c validation.

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -1,7 +1,8 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+            "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
-    <meta charset='utf-8'>
+    <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
     <title>@yield('title') </title>
     <link rel="stylesheet" type="text/css" href="{{URL::asset('css/p3.css')}}">
 </head>
